f073921eefbb369c7d57ed08b7d9b54b1501c806
Resakim <65197613+Resakim@users.noreply.github.com>
2020-10-05 05:21:22 +0900

    Add files via upload

---
+++ discordbit.py
@@ -0,0 +1,20 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("login")
+    print(client.user.name)
+    print(client.user.id)
+    print("─────────────────────")
+    activity = discord.Game(name="산에서 살어리랏다")
+    await client.change_presence(status=discord.Status.idle, activity=activity)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!안녕"):
+        await client.send_message(message.channel, "안녕하세요")
+
+client.run("NjMzMzI4MjE5MzE0OTc4ODI2.XaSW3g.vg0t32jbzUO8dSAdpa4cAp3_zuM")
