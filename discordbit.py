4228fa39e63f2ea0b984fd7c51007f4c7a4a1436
Resakim <65197613+Resakim@users.noreply.github.com>
2020-10-08 04:00:36 +0900

    Update discordbit.py

--- discordbit.py
+++ discordbit.py
@@ -1,27 +1,26 @@
 import discord
-import os
 
-client = discord.Client()
 
-@client.event
-async def on_ready():
-    print("login")
-    print(client.user.name)
-    print(client.user.id)
-    print("─────────────────────")
-    game = discord.Game("산에서 살어리랏다")
-    await client.change_presence(status=discord.Status.online, activity=game)
-    #await client.change_presence(activity=discord.Game(name="산에서 살어리랏다"))
-    #await client.change_presence(game=discord.Game(name='산에서 살어리랏다', type=1))
-    
-@client.event
-async def on_message(self, message):
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+        print("login")
+        print(self.user.name)
+        print(self.user.id)
+        print("─────────────────────")
+        game = discord.Game("산에서 살어리랏다")
+        await self.change_presence(status=discord.Status.online, activity=game)
+
+    async def on_message(self, message):
         # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
 
         if message.content.startswith('안녕'):
-            await message.channel.send('안녕하세요 {0.author.mention}님'.format(message))
-        
+            await message.channel.send('안녕하세요 {0.author.mention} 님'.format(message))
+
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
