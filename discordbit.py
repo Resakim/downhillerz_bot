fd086ebaa7f005b4afc6263dcc16f5526878a7cf
Resakim <65197613+Resakim@users.noreply.github.com>
2020-10-08 03:56:57 +0900

    Update discordbit.py

--- discordbit.py
+++ discordbit.py
@@ -14,11 +14,14 @@ async def on_ready():
     #await client.change_presence(activity=discord.Game(name="산에서 살어리랏다"))
     #await client.change_presence(game=discord.Game(name='산에서 살어리랏다', type=1))
     
-
 @client.event
-async def on_message(message):
-    if message.content.startswith("안녕"):
-	    await message channel.send("안녕하세요")
+async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('안녕'):
+            await message.channel.send('안녕하세요 {0.author.mention}님'.format(message))
         
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
