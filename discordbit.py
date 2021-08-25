20b41a4795ffad78338608a3dc39276399654eb6
Resakim <65197613+Resakim@users.noreply.github.com>
2021-08-25 20:20:54 +0900

    Update discordbit.py

--- discordbit.py
+++ discordbit.py
@@ -14,7 +14,13 @@ async def on_ready():
     activity = discord.Game(name="집에서 교육")
     await client.change_presence(status=discord.Status.online, activity=activity)
     
-
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if "하위" in message.content:
+        await message.channel.send("헤위~")
+        return
 
 
 access_token = os.environ["BOT_TOKEN"]
